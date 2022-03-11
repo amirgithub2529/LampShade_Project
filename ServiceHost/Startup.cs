2cd10a897f37bed346b605afb25b31624542141b
amirgithub2529 <amirnj2529@gmail.com>
2022-03-11 15:48:31 +0330

    Added-AdminLayout-and-Index-(Video-10-seoson-11-productCategory)

--- ServiceHost/Startup.cs
+++ ServiceHost/Startup.cs
@@ -50,6 +50,7 @@ namespace ServiceHost
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                //endpoints.MapDefaultControllerRoute();
             });
         }
     }

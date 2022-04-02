0b69ac3ede1527cb1bb4a8ef75ae2564a4d16e78
amirgithub2529 <amirnj2529@gmail.com>
2022-04-02 19:33:04 +0430

    CustomerDiscount Management Implemented | ColleagueDiscount Management core Implemented

---
+++ DiscountManagement.Application/ColleagueDiscountApplication.cs
@@ -0,0 +1,95 @@
+﻿using _0_Framework.Application;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
+using System.Collections.Generic;
+
+namespace DiscountManagement.Application
+{
+    public class ColleagueDiscountApplication : IColleagueDiscountApplication
+    {
+        private readonly IColleagueDiscountRepository _colleagueDiscountRepository;
+
+        public ColleagueDiscountApplication(IColleagueDiscountRepository colleagueDiscountRepository)
+        {
+            _colleagueDiscountRepository = colleagueDiscountRepository;
+        }
+
+        public OperationResult Define(DefineColleagueDiscount command)
+        {
+            OperationResult operation = new OperationResult();
+
+            if (_colleagueDiscountRepository.Exists(x => x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var ColleagueDiscount = new ColleagueDiscount(command.ProductId, command.DiscountRate);
+            _colleagueDiscountRepository.Create(ColleagueDiscount);
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+
+
+        public OperationResult Edit(EditColleagueDiscount command)
+        {
+            OperationResult operation = new OperationResult();
+
+            if (_colleagueDiscountRepository.Exists(x => x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate && x.Id != command.Id))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var ColleagueDiscount = _colleagueDiscountRepository.Get(command.Id);
+
+            if(ColleagueDiscount == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            ColleagueDiscount.Edit(command.ProductId, command.DiscountRate);
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+
+
+        public EditColleagueDiscount GetDetails(long id)
+        {
+            return _colleagueDiscountRepository.GetDetails(id);
+        }
+
+
+
+        public OperationResult Remove(long id)
+        {
+            OperationResult operation = new OperationResult();
+
+            var ColleagueDiscount = _colleagueDiscountRepository.Get(id);
+
+            if (ColleagueDiscount == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            ColleagueDiscount.Remove();
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+
+
+        public OperationResult Restore(long id)
+        {
+            OperationResult operation = new OperationResult();
+
+            var ColleagueDiscount = _colleagueDiscountRepository.Get(id);
+
+            if (ColleagueDiscount == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            ColleagueDiscount.Restore();
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+
+
+        public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            return _colleagueDiscountRepository.Search(searchModel);
+        }
+    }
+}
--- ShopManagement.Infrastructure.EFCore/Repository/ProductRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repository/ProductRepository.cs
@@ -3,12 +3,8 @@ using _0_Framework.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using ShopManagement.Application.Contracts.Product;
 using ShopManagement.Domain.ProductAgg;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShopManagement.Infrastructure.EFCore.Repository
 {

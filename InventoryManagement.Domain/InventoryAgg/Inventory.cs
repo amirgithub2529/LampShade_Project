c44b0585bdff58796a50cbecb4ae7972da013467
amirgithub2529 <amirnj2529@gmail.com>
2022-04-05 12:02:14 +0430

    IInventoryApplication Implemented

--- InventoryManagement.Domain/InventoryAgg/Inventory.cs
+++ InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -22,8 +22,14 @@ namespace InventoryManagement.Domain.InventoryAgg
             CreationDate = DateTime.Now;
         }
 
+        public void Edit(long productId, double unitPrice)
+        {
+            ProductId = productId;
+            UnitPrice = unitPrice;          
+        }
 
-        private long CalculateCurrentCount()
+
+        public long CalculateCurrentCount()
         {
             var plus = Operations.Where(x => x.Operation).Sum(x => x.Count);
             var minus = Operations.Where(x => !x.Operation).Sum(x => x.Count);
@@ -46,34 +52,6 @@ namespace InventoryManagement.Domain.InventoryAgg
             var operation = new InventoryOperation(false, count, operatorId, currentCount, description, orderId, Id);
             Operations.Add(operation);
             InStock = currentCount > 0;
-        }
-    }
-
-
-    public class InventoryOperation
-    {
-        public long Id { get; private set; }
-        public bool Operation { get; private set; } //ورودی به انبار =1  و  خروجی از انبار =0
-        public long Count { get; private set; }
-        public long OperatorId { get; private set; }
-        public DateTime OperationDate { get; private set; }
-        public long CurrentCount { get; private set; }
-        public string Description { get; private set; }
-        public long OrderId { get; private set; }
-        public long InventoryId { get; private set; }
-        public Inventory Inventory { get; private set; }
-
-        public InventoryOperation(bool operation, long count, long operatorId,
-            long currentCount, string description, long orderId, long inventoryId)
-        {
-            Operation = operation;
-            Count = count;
-            OperatorId = operatorId;
-            CurrentCount = currentCount;
-            Description = description;
-            OrderId = orderId;
-            InventoryId = inventoryId;
-            OperationDate = DateTime.Now;
         }
     }
 }
